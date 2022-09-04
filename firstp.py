54bdca66284f0dbfb802d59ead199d7e1c082086
Arash Hamzehkhani <arashhamzehkhani3@gmail.com>
2022-09-04 14:22:44 +0430

    add driver

--- firstp.py
+++ firstp.py
@@ -1,8 +1,9 @@
 class Car:
 
-    def __init__(self,name="BMW",model="x33"):
+    def __init__(self,name="BMW",model="x33", driver="user"):
         self.name=name
         self.model=model
+        self.driver=driver
 
     
     def __str__(self):
@@ -10,10 +11,26 @@ class Car:
         return(f"{self.name},model is:,{self.model}")
 
 
+
+
+
     def start(self,fuel):
         self.fuel=fuel
 
         print(f"Engine have started with{self.fuel}")
+
+
+
+
+
+
+    
+    def get_name(self,drive):
+       self.drive=drive
+       drivername={}
+       drivername["name"]=self.drive
+       print("This car is driven by ",drivername["name"])
+
 
 
 
@@ -27,14 +44,10 @@ class Car:
 def main():
     uname=input("Enter name of  the car:")
     model=input("Enter the model:")
-    car1=Car(uname,model)
-    print(car1)
-    car1.start(40)
-
-
-
-
-
+    driver=input("what is your name")
+    car1=Car(uname,model,driver)
+    car1.get_name(driver)
+    car1.start(40) 
 
 
 
